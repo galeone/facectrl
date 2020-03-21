a8133268d0261aeb1c72dd91f85c16e6a88cb997
Paolo Galeone <nessuno@nerdz.eu>
2020-03-21 19:42:32 +0100

    ctrl: it works - but only in debug. Something strange happens because/thanks to opencv imshow

--- facectrl/ctrl.py
+++ facectrl/ctrl.py
@@ -9,15 +9,16 @@
 import json
 import logging
 import sys
-import time
+import threading
 from argparse import ArgumentParser
 from pathlib import Path
-from threading import Lock
+from time import sleep, time
 from typing import Tuple
 
 import cv2
+import numpy as np
 import tensorflow as tf
-from gi.repository import GLib, Playerctl
+from gi.repository import GLib, GObject, Playerctl
 
 from facectrl.ml import (ClassificationResult, Classifier, FaceDetector,
                          Thresholds)
@@ -25,7 +26,7 @@ from facectrl.video import Tracker, VideoStream
 
 
 class Controller:
-    """Controls everything that passes on the bus and on the camera.
+    """The controller. It controls your player, tracks and classify your face.
     This is a Glib application"""
 
     def __init__(
@@ -37,7 +38,7 @@ class Controller:
         metric_name: str,
         debug: bool,
     ) -> None:
-        """The controller, that controles the player using the models."""
+        """The controller. It controls your player, tracks and classify your face."""
         self._desired_player = player_name
         self._manager = Playerctl.PlayerManager()
         self._manager.connect("name-appeared", self._on_name_appeared)
@@ -46,6 +47,11 @@ class Controller:
         self._stream = stream
         self._detector = detector
         self._debug = debug
+        self._expansion = (70, 70)
+        self._counters = {hit: 0 for hit in ClassificationResult}
+        # FPS value of this application. It has nothing to the with the
+        # VideoStram.fps value.
+        self._fps = -1
 
         with open(
             logdir / "on" / "best" / metric_name / (metric_name + ".json")
@@ -67,19 +73,69 @@ class Controller:
             model=model, thresholds=thresholds, debug=self._debug,
         )
 
-        self._playing = False
+        # These 3 bools control the player using the camera
+        self._play = False
         self._stop = False
-        self._locks = {"stop": Lock(), "playing": Lock()}
+        self._pause = False
+        # However, we need to let the user control the player
+        # thus we set another flag that is set only if the play(),pause(),stop()
+        # methods have been called from this application.
+        self._internal = False
 
         # Start main loop
         glib_loop = GLib.MainLoop()
         glib_loop.run()
 
+    def _fps_value(self):
+        if self._fps < 0:
+            # Here the best guess for a second
+            # is the VideoStream FPS value.
+            # However, this will last longer than one
+            # second for sure, since the VideStream FPS value
+            # doens't take into account the image processing
+            # time.
+            return self._stream.fps
+        return self._fps
+
+    def _reset(self):
+        self._play = False
+        self._stop = False
+        self._pause = False
+        self._player = None
+
+    def _pause_player(self):
+        self._internal = True
+        if self._player:
+            try:
+                self._player.pause()
+            except GLib.Error:
+                self._reset()
+        else:
+            self._reset()
+
+    def _play_player(self):
+        self._internal = True
+        if self._player:
+            try:
+                self._player.play()
+            except GLib.Error:
+                self._reset()
+        else:
+            self._reset()
+
+    def _stop_player(self):
+        self._internal = True
+        if self._player:
+            try:
+                self._player.stop()
+            except GLib.Error:
+                self._reset()
+        else:
+            self._reset()
+
     def _on_name_vanished(self, manager, name) -> None:
-        self._locks["stop"].acquire()
         logging.info("Player vanising...")
-        self._stop = True
-        self._locks["stop"].release()
+        self._reset()
 
     def _on_name_appeared(self, manager, name) -> None:
         if name.name != self._desired_player:
@@ -88,31 +144,36 @@ class Controller:
             )
             return
         self._player = Playerctl.Player.new_from_name(name)
-
         self._player.connect("playback-status::playing", self._on_play)
         self._player.connect("playback-status::paused", self._on_pause)
         self._player.connect("playback-status::stopped", self._on_stop)
-
         self._manager.manage_player(self._player)
         self._start()
 
     def _on_play(self, player, status) -> None:
-        logging.info("[PLAY] Setting to True")
-        self._locks["playing"].acquire()
-        self._playing = True
-        self._locks["playing"].release()
-
-    def _on_pause(self, player, status) -> None:
-        logging.info("[PAUSE] Setting to False")
-        self._locks["playing"].acquire()
-        self._playing = False
-        self._locks["playing"].release()
+        logging.info("ON PLAY")
+        if self._internal:
+            self._internal = False
+            self._play = True
+            self._stop = False
+            self._pause = False
 
     def _on_stop(self, player, status) -> None:
-        logging.info("[STOP] Setting to False")
-        self._locks["playing"].acquire()
-        self._playing = False
-        self._locks["playing"].release()
+        logging.info("ON STOP")
+        if self._internal:
+            self._internal = False
+            self._play = False
+            self._stop = True
+            self._pause = False
+
+    def _on_pause(self, player, status) -> None:
+        logging.info("ON PAUSE")
+        if self._internal:
+            self._internal = False
+
+            self._play = False
+            self._stop = False
+            self._pause = True
 
     def _detect_and_classify(self, frame) -> Tuple[ClassificationResult, Tuple]:
         bounding_box = self._detector.detect(frame)
@@ -123,101 +184,125 @@ class Controller:
 
             classification_result = self._classifier(
                 Classifier.preprocess(
-                    FaceDetector.crop(frame, bounding_box, expansion=(70, 70))
+                    FaceDetector.crop(frame, bounding_box, expansion=self._expansion)
                 )
             )
 
         return classification_result[0], bounding_box
 
-    def _decide(self, classification_result) -> None:
-        self._locks["stop"].acquire()
-        if not self._stop:
-            if (
-                not self._is_playing()
-                and classification_result == ClassificationResult.HEADPHONES_ON
-            ):
-                logging.info("PLAY")
-                self._player.play()
-            elif (
-                self._is_playing()
-                and classification_result == ClassificationResult.HEADPHONES_OFF
-            ):
-                logging.info("PAUSE")
-                self._player.pause()
-        self._locks["stop"].release()
+    def _decide(
+        self,
+        classification_result: ClassificationResult,
+        previous_result: ClassificationResult,
+    ) -> None:
+        if classification_result == previous_result:
+            self._counters[classification_result] += 1
+        else:
+            for hit in ClassificationResult:
+                self._counters[hit] = 0
+
+        # Stabilize the predictions: take a NEW action only if all the latest
+        # predictions agree with this one
+        if self._counters[classification_result] >= self._fps_value():
+            if self._is_playing():
+                logging.info("is playing")
+                if classification_result in (
+                    ClassificationResult.HEADPHONES_OFF,
+                    ClassificationResult.UNKNOWN,
+                ):
+                    logging.info("decide: pause")
+                    self._pause_player()
+                    return
+            elif self._is_stopped() or self._is_paused():
+                logging.info("is stopped or paused")
+                if classification_result == ClassificationResult.HEADPHONES_ON:
+                    logging.info("decide: play")
+                    self._play_player()
+                    return
+
+        logging.info("decide: dp nothing")
 
     def _is_stopped(self) -> bool:
-        self._locks["stop"].acquire()
-        stop = self._stop
-        self._locks["stop"].release()
-        return stop
+        return self._stop
 
     def _is_playing(self) -> bool:
-        self._locks["playing"].acquire()
-        playing = self._playing
-        self._locks["playing"].release()
-        return playing
+        return self._play
+
+    def _is_paused(self) -> bool:
+        return self._is_paused
 
     def _start(self) -> None:
         with self._stream:
-            while not self._is_stopped():
+            while not self._is_stopped() and self._player:
                 classification_result = ClassificationResult.UNKNOWN
                 # find the face for the first time in this loop
                 logging.info("Detecting a face...")
-                start = time.time()
-                while classification_result is ClassificationResult.UNKNOWN:
+                start = time()
+                while (
+                    classification_result == ClassificationResult.UNKNOWN
+                    and self._player
+                ):
                     frame = self._stream.read()
 
                     # More than 2 seconds without a detected face: pause
                     if (
                         self._is_playing()
-                        and time.time() - start > 2
+                        and time() - start > 2
                         and not self._is_stopped()
                     ):
-                        logging.info("Nobody in front of the camera (?)")
-                        self._player.pause()
+                        logging.info("Nobody in front of the camera")
+                        self._pause_player()
+                        sleep(1)
 
                     classification_result, bounding_box = self._detect_and_classify(
                         frame
                     )
 
+                previous_result = classification_result
+                self._decide(classification_result, previous_result)
+
                 tracker = Tracker(
                     frame,
                     bounding_box,
-                    max_failures=self._stream.fps,
+                    # Halve the FPS value because at this time, self.fps
+                    # Is the VideoStream FPS - without taking into account the
+                    # image processing.
+                    max_failures=self._fps_value() // 2,
                     debug=self._debug,
                 )
                 tracker.classifier = self._classifier
 
-                self._decide(classification_result)
-
                 try:
                     # Start tracking stream
-                    while not self._is_stopped():
+                    frame_counter = 0
+                    while not self._is_stopped() and self._player:
+                        if self._fps < 0:
+                            start = time()
                         frame = self._stream.read()
-                        classification_result = tracker.track_and_classify(frame)
-                        self._decide(classification_result)
+                        classification_result = tracker.track_and_classify(
+                            frame, expansion=self._expansion
+                        )
+                        self._decide(classification_result, previous_result)
+                        previous_result = classification_result
+                        if self._fps < 0:
+                            frame_counter += 1
+                            if time() - start >= 1:
+                                self._fps = frame_counter
+                                # Update tracker max_failures
+                                tracker.max_failures = self._fps_value()
 
                 except ValueError:
                     logging.info("Tracking failed")
-                else:
-                    logging.info("Unable to classify the input")
+                    self._pause_player()
 
         # Get ready to restart
-        self._locks["stop"].acquire()
-        self._locks["playing"].acquire()
-        self._playing = False
-        self._stop = False
-        self._locks["stop"].release()
-        self._locks["playing"].release()
         if self._debug:
             cv2.destroyAllWindows()
+        self._reset()
 
 
 def main():
     """Main method, invoked with python -m facectrl.ctrl."""
-    logging.basicConfig(level=logging.INFO)
-
     parser = ArgumentParser()
     parser.add_argument("--player", required=True, type=str)
     parser.add_argument("--logdir", required=True, type=str)
@@ -226,6 +311,9 @@ def main():
     parser.add_argument("--metric", required=True)
     parser.add_argument("--debug", default=False, action="store_true")
     args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.INFO)
 
     stream = VideoStream(args.stream_source)
     detector = FaceDetector(Path(args.classifier_params))
